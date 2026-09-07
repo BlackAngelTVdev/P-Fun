03c9588246e8bcade8f62986bf420c07810197a2
Damien Rochat <dami.scoot3@gmail.com>
2026-09-07 08:34:54 +0200

    refactor(gui): génère les checkboxes des séries avec LINQ
    [30min] [DONE]

--- P-Fun/mainPage.Designer.cs
+++ P-Fun/mainPage.Designer.cs
@@ -19,11 +19,6 @@ namespace P_Fun
         {
             this.plotPanel = new System.Windows.Forms.Panel();
             this.sidePanel = new System.Windows.Forms.Panel();
-            this.chkBTC = new System.Windows.Forms.CheckBox();
-            this.chkETH = new System.Windows.Forms.CheckBox();
-            this.chkBNB = new System.Windows.Forms.CheckBox();
-            this.chkSOL = new System.Windows.Forms.CheckBox();
-            this.chkXRP = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // plotPanel
@@ -37,76 +32,11 @@ namespace P_Fun
             // 
             // sidePanel
             // 
-            this.sidePanel.Controls.Add(this.chkXRP);
-            this.sidePanel.Controls.Add(this.chkSOL);
-            this.sidePanel.Controls.Add(this.chkBNB);
-            this.sidePanel.Controls.Add(this.chkETH);
-            this.sidePanel.Controls.Add(this.chkBTC);
             this.sidePanel.Dock = System.Windows.Forms.DockStyle.Right;
             this.sidePanel.Location = new System.Drawing.Point(600, 0);
             this.sidePanel.Name = "sidePanel";
             this.sidePanel.Size = new System.Drawing.Size(200, 450);
             this.sidePanel.TabIndex = 1;
-            // 
-            // chkBTC
-            // 
-            this.chkBTC.AutoSize = true;
-            this.chkBTC.Checked = true;
-            this.chkBTC.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkBTC.Location = new System.Drawing.Point(20, 20);
-            this.chkBTC.Name = "chkBTC";
-            this.chkBTC.Size = new System.Drawing.Size(75, 19);
-            this.chkBTC.TabIndex = 0;
-            this.chkBTC.Text = "BTC/USDT";
-            this.chkBTC.UseVisualStyleBackColor = true;
-            // 
-            // chkETH
-            // 
-            this.chkETH.AutoSize = true;
-            this.chkETH.Checked = true;
-            this.chkETH.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkETH.Location = new System.Drawing.Point(20, 50);
-            this.chkETH.Name = "chkETH";
-            this.chkETH.Size = new System.Drawing.Size(76, 19);
-            this.chkETH.TabIndex = 1;
-            this.chkETH.Text = "ETH/USDT";
-            this.chkETH.UseVisualStyleBackColor = true;
-            // 
-            // chkBNB
-            // 
-            this.chkBNB.AutoSize = true;
-            this.chkBNB.Checked = true;
-            this.chkBNB.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkBNB.Location = new System.Drawing.Point(20, 80);
-            this.chkBNB.Name = "chkBNB";
-            this.chkBNB.Size = new System.Drawing.Size(77, 19);
-            this.chkBNB.TabIndex = 2;
-            this.chkBNB.Text = "BNB/USDT";
-            this.chkBNB.UseVisualStyleBackColor = true;
-            // 
-            // chkSOL
-            // 
-            this.chkSOL.AutoSize = true;
-            this.chkSOL.Checked = true;
-            this.chkSOL.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkSOL.Location = new System.Drawing.Point(20, 110);
-            this.chkSOL.Name = "chkSOL";
-            this.chkSOL.Size = new System.Drawing.Size(76, 19);
-            this.chkSOL.TabIndex = 3;
-            this.chkSOL.Text = "SOL/USDT";
-            this.chkSOL.UseVisualStyleBackColor = true;
-            // 
-            // chkXRP
-            // 
-            this.chkXRP.AutoSize = true;
-            this.chkXRP.Checked = true;
-            this.chkXRP.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkXRP.Location = new System.Drawing.Point(20, 140);
-            this.chkXRP.Name = "chkXRP";
-            this.chkXRP.Size = new System.Drawing.Size(77, 19);
-            this.chkXRP.TabIndex = 4;
-            this.chkXRP.Text = "XRP/USDT";
-            this.chkXRP.UseVisualStyleBackColor = true;
             // 
             // mainPage
             // 
@@ -124,10 +54,5 @@ namespace P_Fun
 
         private System.Windows.Forms.Panel plotPanel;
         private System.Windows.Forms.Panel sidePanel;
-        private System.Windows.Forms.CheckBox chkBTC;
-        private System.Windows.Forms.CheckBox chkETH;
-        private System.Windows.Forms.CheckBox chkBNB;
-        private System.Windows.Forms.CheckBox chkSOL;
-        private System.Windows.Forms.CheckBox chkXRP;
     }
-}
+}
--- P-Fun/mainPage.cs
+++ P-Fun/mainPage.cs
@@ -1,10 +1,30 @@
+using System.Drawing;
+
 namespace P_Fun
 {
     public partial class mainPage : Form
     {
+        private static readonly string[] SeriesPairs =
+            ["BTC/USDT", "ETH/USDT", "BNB/USDT", "SOL/USDT", "XRP/USDT"];
+
         public mainPage()
         {
             InitializeComponent();
+            BuildSeriesCheckBoxes();
+        }
+
+        private void BuildSeriesCheckBoxes()
+        {
+            SeriesPairs
+                .Select((pair, index) => new CheckBox
+                {
+                    Text = pair,
+                    AutoSize = true,
+                    Checked = true,
+                    Location = new Point(20, 20 + index * 30),
+                })
+                .ToList()
+                .ForEach(sidePanel.Controls.Add);
         }
     }
-}
+}
